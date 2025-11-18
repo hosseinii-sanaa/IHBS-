8b16c52e3b74a205d8d0dfe434f89cec6c3e8915
sana hosseini <hosseinii.sanaa@gmail.com>
2025-11-18 15:27:32 +0330

    Delete rhbsirdata directory

--- rhbsirdata/R/global_variables.R
+++
@@ -1,2 +0,0 @@
-# Declare global variables to avoid R CMD check notes
-globalVariables(c("ID", "year", "Expenditure", "Income", "."))
--- rhbsirdata/R/imports.R
+++
@@ -1,5 +0,0 @@
-#' @importFrom utils capture.output head str
-#' @importFrom stats setNames
-#' @importFrom rlang := .data
-#' @importFrom tibble as_tibble
-NULL
